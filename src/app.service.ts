72bb6d39a3bdee3f2ec5056af788321341579a7d
fredydlemus <fflores@eaglehubs.com>
2024-07-03 09:15:25 -0600

    added more

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'This is a simple NestJS API!';
+    return 'This is a simple NestJS API!!!';
   }
 }
