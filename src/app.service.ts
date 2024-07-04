8fd0f8b95cfd209cd707d3b058985931572b465b
fredydlemus <fflores@eaglehubs.com>
2024-07-03 19:33:59 -0600

    added new greeting endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Get } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'This is a simple NestJS API!!!';
+  }
+
+  @Get('greetings')
+  anotherGreetings(): string {
+    return 'Hello from another greetings';
   }
 }
